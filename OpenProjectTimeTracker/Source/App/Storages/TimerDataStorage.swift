669bd0f07f13948de39b02c7993434d273703975
shtangey <denis.shtangey@gmail.com>
2022-08-11 11:28:41 +0200

    Refactor project structure

---
+++ OpenProjectTimeTracker/Source/App/Storages/TimerDataStorage.swift
@@ -0,0 +1,16 @@
+//
+//  TimerDataStorage.swift
+//  OpenProjectTimeTracker
+//
+//  Created by Denis Shtangey on 11.08.22.
+//
+
+import Foundation
+
+protocol TimerDataStorageProtocol {
+    
+}
+
+final class TimerDataStorage: TimerDataStorageProtocol {
+    
+}
